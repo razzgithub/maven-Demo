6b2f70bf5c374f68153257b6fe60bfa22435680a
BIKRAM <BIKRAM@LAPTOP-V2KAIUF1>
2022-03-23 17:58:17 +0530

    3rd commit

--- com.crm.vtiger.testcases/src/test/java/com/crm/vtiger/testcases/DemoTest.java
+++ com.crm.vtiger.testcases/src/test/java/com/crm/vtiger/testcases/DemoTest.java
@@ -3,10 +3,22 @@
  */
 package com.crm.vtiger.testcases;
 
+import org.testng.annotations.Test;
+
+
 /**
  * @author BIKRAM
  *
  */
 public class DemoTest {
+	
+	@Test
+	public void check() {
+		
+		System.out.println("check successfully"); 
+		System.out.println("Vivek-Shashi");
+		
+		
+	}
 
 }
