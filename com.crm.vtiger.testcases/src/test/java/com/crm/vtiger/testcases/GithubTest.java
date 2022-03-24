cbe1ec9d8a69bde32e29e45f9ea49c31560670bd
BIKRAM <BIKRAM@LAPTOP-V2KAIUF1>
2022-03-24 14:07:05 +0530

    user 1 Created github Test Scripts

---
+++ com.crm.vtiger.testcases/src/test/java/com/crm/vtiger/testcases/GithubTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.crm.vtiger.testcases;
+
+/**
+ * @author BIKRAM
+ *
+ */
+public class GithubTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("user 1 created github");
+		
+	}
+
+
+}
